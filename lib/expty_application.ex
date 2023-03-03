030f44d739aadb72a61a3bd1a4382051a72ee91f
Cocoa <r.xu.1@research.gla.ac.uk>
2023-03-03 13:55:56 -0800

    conditionally register smartcell based on `:os.type`

--- lib/expty_application.ex
+++ lib/expty_application.ex
@@ -3,7 +3,12 @@ defmodule ExPTY.Application do
   use Application
 
   def start(_type, _args) do
-    if Code.ensure_loaded?(Kino), do: Kino.SmartCell.register(ExPTY.SmartCell)
+    case :os.type() do
+      {:win32, _} ->
+        nil
+      _ ->
+        if Code.ensure_loaded?(Kino), do: Kino.SmartCell.register(ExPTY.SmartCell)
+    end
     Supervisor.start_link([], strategy: :one_for_one)
   end
 end
